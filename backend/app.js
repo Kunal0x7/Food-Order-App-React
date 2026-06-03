932e2b9910428e6e12f041d5359c1810f3673a8e
Kunal Ranjan <kunal.singh.07007@gmail.com>
2026-06-03 21:45:02 +0530

    managed env port on backend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import express from 'express';
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -66,4 +67,4 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
-app.listen(3000);
+app.listen(port);
