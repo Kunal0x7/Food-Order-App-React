9dcd7bb257b921442ee3835fc457e4c20e9a3626
Kunal Ranjan <kunal.singh.07007@gmail.com>
2026-06-03 10:15:32 +0530

    Handled Error and Loading State Ui

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,8 +1,15 @@
 import useHttp from "../Hooks/useHttp";
 import MealItem from "./MealItem";
+import Error from "./Error";
 const requestConfig = {};
 export default function Meals() {
   const{data:loadedmeals,isLoading,error}=useHttp('http://localhost:3000/meals', requestConfig, []);
+  if(isLoading){
+    return <p className="center">Loading...</p>
+  }
+  if(error){
+    return <Error title="Failed to fetch meals" message={error} />
+  }
   
   return (
     <ul id="meals">
